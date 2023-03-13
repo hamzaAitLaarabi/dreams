196c74b0532202c3fd61cee4f4c0862a2161fc0a
hamzaLaarabi <hamzalaarabi04@gmail.com>
2023-03-13 23:50:05 +0100

    add env.example

--- startup.sh
+++ startup.sh
@@ -3,9 +3,7 @@
 # make sure you've copied the default file from the nginx site-enabled and edited the root directory to 
 #/home/site/wwwroot/public
 # add "try_files $uri $uri/ /index.php?$args" in /etc/nginx/sites-available/default server location;
-cp /etc/nginx/sites-available/default /home/site/default
 service nginx reload
-
 cd /home/site/wwwroot && cp -n .env.example .env 
 php artisan key:generate
 service nginx reload
